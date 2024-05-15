0f1ee3cd1f048d75c32632185ddd6560f7c9dd72
Ozge <email>
2024-05-15 16:16:33 +0300

    pratice

---
+++ src/test/java/automationpractice/LoginUserWithCorrectEmailAndPassword.java
@@ -0,0 +1,54 @@
+package automationpractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LoginUserWithCorrectEmailAndPassword {
+    @Test
+    public void test01() {
+
+//    1. Launch browser
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+//2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+//            3. Verify that home page is visible successfully
+        WebElement title=driver.findElement(By.xpath("(//span[text()='Automation'])[1]"));
+        Assertions.assertTrue(title.isDisplayed());
+//4. Click on 'Signup / Login' button
+     driver.findElement(By.cssSelector("i[class='fa fa-lock']")).click();
+//5. Verify 'Login to your account' is visible
+WebElement login=driver.findElement(By.xpath("//h2[.='Login to your account']"));
+Assertions.assertTrue(login.isDisplayed());
+//6. Enter correct email address and password
+        WebElement emailBox=driver.findElement(By.cssSelector("input[type='email']"));
+        emailBox.sendKeys("ozge01@gmail.com");
+
+        WebElement passwordBox=driver.findElement(By.name("password"));
+        passwordBox.sendKeys("123");
+
+//7. Click 'login' button
+driver.findElement(By.xpath("//button[text()='Login']")).click();
+
+//8. Verify that 'Logged in as username' is visible
+
+        WebElement loogedAs=     driver.findElement(By.cssSelector("i[class='fa fa-lock']"));
+        Assertions.assertTrue(loogedAs.isDisplayed());
+
+//9. Click 'Delete Account' button
+        driver.findElement(By.xpath("//i[@class='fa fa-trash-o']")).click();
+//10. Verify that 'ACCOUNT DELETED!' is visible
+        WebElement deleted=driver.findElement(By.cssSelector("h2[class='title text-center']"));
+        Assertions.assertTrue(deleted.isDisplayed());
+
+
+    }
+
+}
--- src/test/java/practice02/Q1_BeforeEach_fillForm.java
+++ src/test/java/practice02/Q1_BeforeEach_fillForm.java
@@ -1,5 +1,6 @@
 package practice02;
 
+import com.github.javafaker.Faker;
 import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
@@ -15,34 +16,16 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Q1_BeforeEach_fillForm {
-
-    /*
-Create chrome driver by using @BeforeEach annotation and WebDriverManager
-Go to url : http://www.gmibank.com/
-Click on 'User Icon'
-Click on 'Register'
-Enter SSN
-Enter First Name
-Enter Last Name
-Enter Address
-Enter Phone Number
-Enter Username
-Enter Email
-Enter New password
-Enter New password confirmation
-Click on Register button
-Assert that user registered
-Close the browser by using @AfterEach annotation
-
-     */
-
     WebDriver driver;  // class variable
+
+    Faker faker = new Faker();
+
     //        Create chrome driver by using @BeforeEach annotation and WebDriverManager
 
     @BeforeEach
     public void setUp(){
         // WebDriverManager was used to set up the system for drivers
-        WebDriverManager.chromedriver().setup();
+        // WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // Implicit wait only waits when it's needed
@@ -70,70 +53,75 @@ Close the browser by using @AfterEach annotation
         // Verify using if statement
 
         String url = driver.getCurrentUrl();
-        if (url.contains("register1")){
-            System.out.println("Register option is clicked successfully!");
-        } else {
-            System.out.println("Register option is not clicked successfully!");
-        }
+//        if (url.contains("register")){
+//            System.out.println("Register option is clicked successfully!");
+//        } else {
+//            System.out.println("Register option is not clicked successfully!");
+//        }
 
-        //assertTrue(url.contains("register"));  // Hard Assertion
+        // If statements can NOT be used for Assertions
 
-        WebElement ssnBox=driver.findElement(By.xpath("//input[@id='ssn']"));
-        ssnBox.sendKeys("1231555411");
-
-        WebElement firstNameBox=driver.findElement(By.xpath("//input[@id='firstname']"));
-        firstNameBox.sendKeys("admin");
-
-        WebElement lastNameBox=driver.findElement(By.xpath("//input[@id='lastname']"));
-        lastNameBox.sendKeys("admin");
-
-        WebElement addressBox=driver.findElement(By.xpath("//input[@id='address']"));
-        addressBox.sendKeys("San jose CR");
-
-
-        WebElement phoneBox=driver.findElement(By.xpath("//*[@id='mobilephone']"));
-        phoneBox.sendKeys("05074366623");
-
-        WebElement usernameBox=driver.findElement(By.xpath("//*[@id='username']"));
-        usernameBox.sendKeys("admin");
-
-
-
-        WebElement emailBox=driver.findElement(By.xpath("//*[@id='email']"));
-        emailBox.sendKeys("ozge_006@gmail.com");
-
-        WebElement passBox=driver.findElement(By.xpath("//*[@id='firstPassword']"));
-        passBox.sendKeys("Aa123456*");
-
-        WebElement passBox2=driver.findElement(By.xpath("//*[@id='secondPassword']"));
-        passBox2.sendKeys("Aa123456*");
-
-        driver.findElement(By.xpath("//*[@id='register-submit']")).click();
-
+        assertTrue(url.contains("register"));  // Hard Assertion
 
 //        Enter SSN
+        WebElement ssn = driver.findElement(By.id("ssn"));
+        ssn.sendKeys("218-93-7728");
+
 //        Enter First Name
+//        driver.findElement(By.id("firstname")).sendKeys("Tom");
+        WebElement firstName =  driver.findElement(By.id("firstname"));
+        firstName.sendKeys(faker.name().firstName());
+
+        Thread.sleep(1000);
 //        Enter Last Name
+        driver.findElement(By.id("lastname")).sendKeys("Hanks");
+        Thread.sleep(1000);
+
 //        Enter Address
+        WebElement address = driver.findElement(By.xpath("//input[@name = 'address']"));
+        address.sendKeys( new Faker().address().fullAddress()); // this way should only be used if you don't need faker class for all input values
+
 //        Enter Phone Number
+        WebElement phoneNum = driver.findElement(By.xpath("//input[@name = 'mobilephone']"));
+        phoneNum.sendKeys(faker.phoneNumber().cellPhone());
 //        Enter Username
+//        driver.findElement(By.cssSelector("[id = 'username']"));  OR
+        WebElement userName = driver.findElement(By.cssSelector("#username"));
+        userName.sendKeys(faker.name().username());
+
 //        Enter Email
+        WebElement email = driver.findElement(By.cssSelector("#email"));
+        email.sendKeys(faker.internet().emailAddress());
+
 //        Enter New password
+        WebElement newPassword =  driver.findElement(By.xpath("//*[@name='firstPassword']"));
+        newPassword.sendKeys("User_2024");
+
 //        Enter New password confirmation
+        WebElement secondPassword =  driver.findElement(By.xpath("//*[@name='secondPassword']"));
+        secondPassword.sendKeys("User_2024");
+
 //        Click on Register button
+//        WebElement registerButton = driver.findElement(By.id("register-submit"));
+        WebElement registerButton = driver.findElement(By.xpath("(//span[.='Register'])[2]"));
+        registerButton.click();
+        Thread.sleep(2000);
+
 //        Assert that user registered
+        WebElement successMessage = driver.findElement(By.xpath("//div[@role='alert']"));
+
+        assertTrue(successMessage.isDisplayed());
+
 //        Close the browser by using @AfterEach annotation
-
-
 
     }
 
 
-//    @AfterEach
-//    public void tearDown() throws InterruptedException {
-//        Thread.sleep(3000); // Hard Wait
-//        driver.close();
-//
-//    }
+    @AfterEach
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000); // Hard Wait
+        driver.close();
+
+    }
 
 }
