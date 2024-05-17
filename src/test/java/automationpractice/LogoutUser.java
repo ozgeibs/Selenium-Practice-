560a0650f10caf0c46f9a77460533c4eeb18c9fc
Ozge <email>
2024-05-17 17:30:51 +0300

    TestCases

---
+++ src/test/java/automationpractice/LogoutUser.java
@@ -0,0 +1,65 @@
+package automationpractice;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LogoutUser {
+    WebDriver driver;
+    @BeforeEach
+    // Launch browser
+    public void setUp(){
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+    }
+
+
+
+
+
+
+
+
+
+//9. Click 'Logout' button
+//10. Verify that user is navigated to login page
+    @Test
+    public void test01(){
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("https://automationexercise.com/");
+        //            3. Verify that home page is visible successfully
+        WebElement category=driver.findElement(By.xpath("//h2[.='Category']"));
+        Assertions.assertTrue(category.isDisplayed());
+        //4. Click on 'Signup / Login' button
+        WebElement signupLoginButton=driver.findElement(By.xpath("//a[.=' Signup / Login']"));
+        signupLoginButton.click();
+        //5. Verify 'Login to your account' is visible
+        WebElement loginText=driver.findElement(By.xpath("//h2[.='Login to your account']"));
+        Assertions.assertTrue(loginText.isDisplayed());
+        //6. Enter correct email address and password
+        WebElement emailBox=driver.findElement(By.xpath("(//input[@name='email'])[1]"));
+        emailBox.sendKeys("ozge@nehaber.com");
+
+        WebElement passBox=driver.findElement(By.xpath("(//input[@name='password'])[1]"));
+        passBox.sendKeys("12345");
+//7. Click 'login' button
+        WebElement loginButton=driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+//8. Verify that 'Logged in as username' is visible
+        WebElement loggedAs=driver.findElement(By.xpath("//*[@class='fa fa-user']"));
+        Assertions.assertTrue(loggedAs.isDisplayed());
+    }
+
+
+}
---
+++ src/test/java/automationpractice/RegisterUserWithExistingEmail.java
@@ -0,0 +1,47 @@
+package automationpractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class RegisterUserWithExistingEmail {
+    @Test
+    public void registerWithExistingEmail(){
+        //    1. Launch browser
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("https://automationexercise.com/");
+        //3. Verify that home page is visible successfully
+        WebElement allHomePage=driver.findElement(By.xpath("(//section[@id='slider'])[1]"));
+        Assertions.assertTrue(allHomePage.isDisplayed());
+        //4. Click on 'Signup / Login' button
+        WebElement signupLogin=driver.findElement(By.xpath("//a[.=' Signup / Login']"));
+        signupLogin.click();
+        //5. Verify 'New User Signup!' is visible
+        WebElement newUserSignup= driver.findElement(By.xpath("(//h2)[3]"));;
+        Assertions.assertTrue(newUserSignup.isDisplayed());
+        //6. Enter name and already registered email address
+        WebElement nameBox=driver.findElement(By.xpath("//input[@type='text']"));
+        nameBox.sendKeys("ozge");
+
+        WebElement emailBox=driver.findElement(By.xpath("(//input[@name='email'])[2]"));
+        emailBox.sendKeys("ozge@nehaber.com");
+        //7. Click 'Signup' button
+        WebElement signupButton=driver.findElement(By.cssSelector("button[data-qa='signup-button']"));
+        signupButton.click();
+
+//8. Verify error 'Email Address already exist!' is visible
+
+        WebElement alreadyExists=driver.findElement(By.xpath("//p[.='Email Address already exist!']"));
+        Assertions.assertTrue(alreadyExists.isDisplayed());
+
+
+    }
+}
